f211af4ec57ccbfc64bc5b2e198e7e69319025fd
gybalog <g.balog15@gmail.com>
2024-05-07 20:01:33 +0200

    Galeria

--- templates/pages/galeria.tpl.php
+++ templates/pages/galeria.tpl.php
@@ -1,1 +1,36 @@
 <h2>Kép Galéria</h2>
+<?php
+    // Alkalmazás logika:
+    include('./includes/config_kepek.inc.php');
+    
+    // adatok összegyűjtése:    
+    $kepek = array();
+    $olvaso = opendir($MAPPA);
+    while (($fajl = readdir($olvaso)) !== false)
+        if (is_file($MAPPA.$fajl)) {
+            $vege = strtolower(substr($fajl, strlen($fajl)-4));
+            if (in_array($vege, $TIPUSOK))
+                $kepek[$fajl] = filemtime($MAPPA.$fajl);            
+        }
+    closedir($olvaso);
+    
+    // Megjelenítés logika:
+?>
+<body>
+    <div id="galeria">
+    <?php
+    arsort($kepek);
+    foreach($kepek as $fajl => $datum)
+    {
+    ?>
+        <div class="kep">
+            <a href="<?php echo $MAPPA.$fajl ?>">
+                <img src="<?php echo $MAPPA.$fajl ?>">
+            </a>            
+            <p>Név:  <?php echo $fajl; ?></p>
+            <p>Dátum:  <?php echo date($DATUMFORMA, $datum); ?></p>
+        </div>
+    <?php
+    }
+    ?>
+</div>
